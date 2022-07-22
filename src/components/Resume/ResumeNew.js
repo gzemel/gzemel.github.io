ef3b59333d9446fa1a16f3a2fc312353e3456a2e
Graham Zemel <69569628+gzemel@users.noreply.github.com>
2022-07-22 11:58:05 -0400

    Revised resume links

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,8 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://cdn.jsdelivr.net/gh/gzemel/resume/GrahamZemel.pdf";
+  
+"https://raw.githubusercontent.com/gzemel/gzemel.github.io/master/src/Assets/GrahamZemel.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
